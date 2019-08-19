a00e67472c4eac400306652da9a795dc20fa667a
ChaiUrs <mysorechaitra@gmail.com>
2019-08-19 23:30:25 +0000

    Completed Queries II-JOIN activity

--- 2_queries_joins/1_total_assignment_duration.sql
+++ 2_queries_joins/1_total_assignment_duration.sql
@@ -1,5 +1,5 @@
 
-SELECT sum(assignments_submissions.duration) as total_duration
-FROM assignments_submissions
+SELECT sum(assignment_submissions.duration) as total_duration
+FROM assignment_submissions
 JOIN students ON students.id = student_id
 WHERE students.name = 'Ibrahim Schimmel';
